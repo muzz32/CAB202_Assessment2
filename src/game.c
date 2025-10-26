6f44df3d59c9669dcad69bbebde0d9ef0b96b55e
Murray Peyton <murraypeyton15@gmail.com>
2025-10-26 23:58:44 +1000

    Final version that was submitted. Final autograder score of 19.40/20. 0.6 marks lost on highscore test

--- src/game.c
+++ src/game.c
@@ -42,7 +42,6 @@ int main(void){
             sequence index is reset. The first step of the sequence is played, and the state
             gets changed to DISPLAY OFF
             */
-            reset_lfsr(&lfsr);
             lfsr.sequence_length++;
             lfsr.sequence_index = 0;
             randnum = step(&lfsr);  
