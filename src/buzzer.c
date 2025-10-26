3dbfc4306e2702503a58ba3a076ee8f9b6322575
Murray Peyton <murraypeyton15@gmail.com>
2025-10-26 19:48:15 +1000

    Throw a lil printf to see what the buzzer is

--- src/buzzer.c
+++ src/buzzer.c
@@ -48,6 +48,7 @@ void set_buzzer(uint8_t index){
     else{
         TCA0.SINGLE.PERBUF = tops[index]; // Period set to the tone in the array
         TCA0.SINGLE.CMP0BUF = tops[index] >> 1; // Compare set to half period for 50% duty cycle
+        printf("%ld", tops[index]);
     }
 }
 
